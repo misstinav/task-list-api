295c73e089095fa1f6eeaec1737dc5149e423865
Valentina Ruiz <v.zruiz06@gmail.com>
2022-11-03 18:00:00 -0700

    pushed through initial table error

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,36 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, make_response
+from app.models.task import Task
+from app import db
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(title=request_body["title"],
+    description=request_body["description"],
+    completed_at=request_body["completed_at"],
+    is_complete = request_body["is_complete"])
+
+    if not new_task.completed_at:
+        new_task.is_complete = False
+        new_task.completed_at = None
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(f"Task {new_task.title} successfully created", 201)
+
+@tasks_bp.route("", methods=["GET"])
+def get_tasks():
+    tasks = Task.query.all()
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append({
+            "id":task.id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.is_complete
+        })
+    return jsonify(tasks_response)
