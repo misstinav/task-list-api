d15e2f48b1bf6c9595d317813431382965afac31
Ryan Thomas <rrthomas1913@gmail.com>
2022-11-06 14:44:02 -0500

    first partner merge

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request, make_response
+from flask import Blueprint, jsonify, request, make_response, abort
 from app.models.task import Task
 from app import db
 
@@ -34,3 +34,47 @@ def get_tasks():
             "is_complete": task.is_complete
         })
     return jsonify(tasks_response)
+
+'''
+Added in please check over: Goal Update & Delete for Wave 1
+'''
+def validate_task(task_id):
+    tasks = Task.query.all()
+    try:
+        task_id = int(task_id)
+    except: 
+        abort(make_response({"message":f"task {task_id} invalid"}, 400))
+
+    for task in tasks:
+        if task.id == task_id:
+            return task_id
+    
+    abort(make_response({"message": f"task {task_id} not found"}, 404))
+
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_task(task_id)
+    
+    
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"Task # {task.id} successfully updated", 200)
+
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return make_response(f"Task #{task_id} successfully deleted")
+
+    
+    
